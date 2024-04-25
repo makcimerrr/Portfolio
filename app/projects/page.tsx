3a46930aee2b48a814cc4b027f3efa9f72be6b79
mdubois <maxou.dub76@gmail.com>
2024-04-25 13:13:43 +0200

    Add projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 import React from "react";
-import projectsData from "../../app/project.json"; // Importez vos données depuis GitHub
+import projectsData from "./project.json"; // Importez vos données depuis GitHub
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
-//import { Article } from "./article";
+import { Article } from "./article";
 import { Redis } from "@upstash/redis";
 import { Eye } from "lucide-react";
 
@@ -11,14 +11,6 @@ const redis = Redis.fromEnv();
 
 export const revalidate = 60;
 export default async function ProjectsPage() {
-  const views = (
-      await redis.mget<number[]>(
-          ...projectsData.map((p) => ["pageviews", "projects", p.slug].join(":")),
-      )
-  ).reduce((acc, v, i) => {
-    acc[projectsData[i].slug] = v ?? 0;
-    return acc;
-  }, {} as Record<string, number>);
 
   const featured = projectsData.find((project) => project.slug === "sample-project")!;
   const top2 = projectsData.find((project) => project.slug === "sample-project")!;
@@ -67,12 +59,6 @@ export default async function ProjectsPage() {
                           <span>SOON</span>
                       )}
                     </div>
-                    <span className="flex items-center gap-1 text-xs text-zinc-500">
-                    <Eye className="w-4 h-4" />{" "}
-                      {Intl.NumberFormat("en-US", { notation: "compact" }).format(
-                          views[featured.slug] ?? 0,
-                      )}
-                  </span>
                   </div>
 
                   <h2
@@ -102,7 +88,7 @@ export default async function ProjectsPage() {
                   .filter((_, i) => i % 3 === 0)
                   .map((project) => (
                       <Card key={project.slug}>
-
+                        <Article project={project} views={[project.slug] ?? 0} />
                       </Card>
                   ))}
             </div>
@@ -111,6 +97,7 @@ export default async function ProjectsPage() {
                   .filter((_, i) => i % 3 === 1)
                   .map((project) => (
                       <Card key={project.slug}>
+                        <Article project={project} views={[project.slug] ?? 0} />
                       </Card>
                   ))}
             </div>
@@ -119,6 +106,7 @@ export default async function ProjectsPage() {
                   .filter((_, i) => i % 3 === 2)
                   .map((project) => (
                       <Card key={project.slug}>
+                        <Article project={project} views={[project.slug] ?? 0} />
                       </Card>
                   ))}
             </div>
