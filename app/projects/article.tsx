63c2b8d90b52ca848a977e1a9e3d681e39e97353
mdubois <maxou.dub76@gmail.com>
2024-06-18 15:43:31 +0200

    :fire: add some projects

--- app/projects/article.tsx
+++ app/projects/article.tsx
@@ -1,42 +1,60 @@
 //import type { Project } from "@/.contentlayer/generated";
 import Link from "next/link";
-import { Eye, View } from "lucide-react";
+import {Eye} from "lucide-react";
 
-type Projects = {     published: boolean;     title: string;     description: string;     date: string;     repository: string;     body: {         raw: string;         code: string;     };     _id: string;     _raw: {         sourceFilePath: string;         sourceFileName: string;         sourceFileDir: string;         contentType: string;         flattenedPath: string;     };     type: string;     path: string;     slug: string; }
+type Projects = {
+    published: boolean;
+    title: string;
+    description: string;
+    date: string;
+    repository: string;
+    _id: string;
+    _raw: {
+        sourceFilePath: string;
+        sourceFileName: string;
+        sourceFileDir: string;
+        contentType: string;
+        flattenedPath: string;
+    };
+    type: string;
+    path: string;
+    slug: string;
+}
 
 type Props = {
-	project: Projects;
-	views: number;
+    project: Projects;
+    views: number;
 };
 
-export const Article: React.FC<Props> = ({ project, views }) => {
-	return (
-		<Link href={`/projects/${project.slug}`}>
-			<article className="p-4 md:p-8">
-				<div className="flex justify-between gap-2 items-center">
-					<span className="text-xs duration-1000 text-zinc-200 group-hover:text-white group-hover:border-zinc-200 drop-shadow-orange">
+export const Article: React.FC<Props> = ({project, views}) => {
+    return (
+        <Link href={`/projects/${project.slug}`}>
+            <article className="p-4 md:p-8">
+                <div className="flex justify-between gap-2 items-center">
+					<span
+                        className="text-xs duration-1000 text-zinc-200 group-hover:text-white group-hover:border-zinc-200 drop-shadow-orange">
 						{project.date ? (
-							<time dateTime={new Date(project.date).toISOString()}>
-								{Intl.DateTimeFormat(undefined, { dateStyle: "medium" }).format(
-									new Date(project.date),
-								)}
-							</time>
-						) : (
-							<span>SOON</span>
-						)}
+                            <time dateTime={new Date(project.date).toISOString()}>
+                                {Intl.DateTimeFormat(undefined, {dateStyle: "medium"}).format(
+                                    new Date(project.date),
+                                )}
+                            </time>
+                        ) : (
+                            <span>SOON</span>
+                        )}
 					</span>
-					<span className="text-zinc-500 text-xs  flex items-center gap-1">
-						<Eye className="w-4 h-4" />{" "}
-						{Intl.NumberFormat("en-US", { notation: "compact" }).format(views)}
+                    <span className="text-zinc-500 text-xs  flex items-center gap-1">
+						<Eye className="w-4 h-4"/>{" "}
+                        {Intl.NumberFormat("en-US", {notation: "compact"}).format(views)}
 					</span>
-				</div>
-				<h2 className="z-20 text-xl font-medium duration-1000 lg:text-3xl text-zinc-200 group-hover:text-white font-display">
-					{project.title}
-				</h2>
-				<p className="z-20 mt-4 text-sm  duration-1000 text-zinc-400 group-hover:text-zinc-200">
-					{project.description}
-				</p>
-			</article>
-		</Link>
-	);
+                </div>
+                <h2 className="z-20 text-xl font-medium duration-1000 lg:text-3xl text-zinc-200 group-hover:text-white font-display">
+                    {project.title}
+                </h2>
+                <p className="z-20 mt-4 text-sm  duration-1000 text-zinc-400 group-hover:text-zinc-200">
+                    {project.description}
+                </p>
+            </article>
+        </Link>
+    );
 };
